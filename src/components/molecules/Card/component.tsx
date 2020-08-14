c6350cbac2016eb34bc0cadd0c913a0ef6368847
MukhtarA <33712120+MukhtarA@users.noreply.github.com>
2020-08-15 00:14:43 +0600

    fix: component Card

--- src/components/molecules/Card/component.tsx
+++ src/components/molecules/Card/component.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from 'react';
-import { Props } from './props';
 import styled from '@emotion/styled';
+import { Props } from './props';
 
-export const Card = styled.div<Props>``;
-
-Card.defaultProps = {
-  background: '#fff',
-  color: '#282828'
-}
+export const Card = styled.div<Props>`
+   ${({ background, color, disableBorder}) => `
+      background-color: ${ '#fff' ||  background };
+      color: ${ '#282828' ||  color };
+      border: ${ disableBorder ? '0' : '1px solid #282828'};
+   `}
+`;
